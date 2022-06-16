04c33f539f8712f6034f9a923e4ddf39dfbb34b0
tcoquery <tcoquery@gmail.com>
2022-06-16 17:49:49 +0200

    add the tasks details on load and creation

--- src/modules/load-tasks.js
+++ src/modules/load-tasks.js
@@ -4,10 +4,12 @@ const allTasks = document.querySelector(".task-list");
 function loadTasks() {
   let jsonString = localStorage.getItem(projectTitle.textContent)
   let retrievedObject = JSON.parse(jsonString);
-  const listItem = document.createElement("li");
-  listItem.textContent = retrievedObject.children[0];
-  allTasks.appendChild(listItem);
+  for(let i = 0; i < retrievedObject.children.length ; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = retrievedObject.children[i].title + " " + retrievedObject.children[i].description + " " + retrievedObject.children[i].date;
+    allTasks.appendChild(listItem);
   }
+}
 
 
 export { loadTasks };
--- src/modules/save-task.js
+++ src/modules/save-task.js
@@ -5,6 +5,9 @@ function saveTask(obj, project) {
   let retrievedObject = JSON.parse(jsonString);
   retrievedObject.children.push(obj);
   localStorage.setItem(project, JSON.stringify(retrievedObject));
+  const listItem = document.createElement("li");
+  listItem.textContent = obj.title + " " + obj.description + " " + obj.date;
+  allTasks.appendChild(listItem);
 };
 
 export {saveTask};
